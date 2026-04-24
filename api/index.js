a27bdfa9b68c08e1dbe8799dd9fe38734245a74e
patidarmk <34210081+patidarmk@users.noreply.github.com>
2026-04-24 18:50:02 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,10 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (req, res) => {
-    let url = req.url;
-
-    // 🔥 IMPORTANT: remove query params
-    url = url.split('?')[0];
+    let url = req.url.split('?')[0];
 
     if (url === '/' || url === '/home') {
         url = '/index.html';
@@ -26,25 +23,15 @@ module.exports = (req, res) => {
 
     const contentType = contentTypeMap[extname] || 'text/plain';
 
-    console.log("Requested URL:", url);
-    console.log("File path:", filePath);
-
     fs.readFile(filePath, (err, content) => {
         if (err) {
-            console.log("Error:", err.code);
+            const notFoundPath = path.join(process.cwd(), 'public', '404.html');
 
-            if (err.code === 'ENOENT') {
-                const notFoundPath = path.join(process.cwd(), 'public', '404.html');
-
-                fs.readFile(notFoundPath, (error, data) => {
-                    res.statusCode = 404;
-                    res.setHeader('Content-Type', 'text/html');
-                    res.end(data || '404 Not Found');
-                });
-            } else {
-                res.statusCode = 500;
-                res.end(`Server Error: ${err.code}`);
-            }
+            fs.readFile(notFoundPath, (error, data) => {
+                res.statusCode = 404;
+                res.setHeader('Content-Type', 'text/html');
+                res.end(data || '404 Not Found');
+            });
         } else {
             res.statusCode = 200;
             res.setHeader('Content-Type', contentType);
